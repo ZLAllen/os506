e877780a42211f636640f42af0e338edc0085d12
zlallen <zhaoqi_li@yahoo.com>
2017-09-04 16:29:59 -0400

    5 syscalls, exit noreturn issues, cat

--- libc/syscalls.c
+++ libc/syscalls.c
@@ -1,14 +1,64 @@
+#include "sysdefs.h"
 #include "syscalls.h"
-#define __NR_write 1
 
-int write(int fd, const char* buf, int count){
+// in x86-64, we can free rcx and r11 for kernel
+
+int read(int fd, char* buf, int size){
+    int ret;
+
+    asm volatile
+        (
+         "syscall"
+         : "=a" (ret)
+         : "0"(__NR_read), "D"(fd), "S"(buf), "d"(size)
+         : "cc", "rcx", "r11", "memory"
+        );
+    return ret;
+}
+
+int write(int fd, const char* buf, int size){
     int ret;
     asm volatile
         (
          "syscall"
          : "=a" (ret)
-         : "0"(__NR_write), "D"(fd), "S"(buf), "d"(count)
+         : "0"(__NR_write), "D"(fd), "S"(buf), "d"(size)
          : "cc", "rcx", "r11", "memory"
         );
     return ret;
+}
+
+int open(const char* file, int flag){
+    int ret;
+    asm volatile
+        (
+         "syscall"
+         : "=a" (ret)
+         : "0"(__NR_open), "D"(file),"S"(flag)
+         : "cc", "rcx", "r11"
+        );
+    return ret;
+}
+
+int close(int fd){
+   int ret;
+
+   asm volatile
+       (
+        "syscall"
+        : "=a" (ret)
+        : "0"(__NR_close), "D"(fd)
+        : "cc", "rcx", "r11"
+       );
+   return ret;
+}
+
+void exit(int status){
+    asm volatile 
+        (
+         "syscall"
+         :
+         : "a"(__NR_exit), "D"(status)
+         : "cc"
+        );
 }
--- libc/syscalls.h
+++ libc/syscalls.h
@@ -1,1 +1,12 @@
-int write(int fd, const char* buf, int count);
+
+
+//open sys flags
+#define O_RDONLY 0x0000
+#define O_WRONLY 0x0001
+
+//more to add
+int open(const char* file, int flag);
+int close(int fd);
+int read(int fd, char* buf, int size);
+int write(int fd, const char* buf, int size);
+void exit(int status);
