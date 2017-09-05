2d864aed4db6189583d7323e1454371f05e2a170
zlallen <zhaoqi_li@yahoo.com>
2017-09-05 01:46:17 -0400

    lost files, need merge

--- libc/syscalls.c
+++ libc/syscalls.c
@@ -10,83 +10,17 @@ int read(int fd, char* buf, int size){
     __asm
         (
          "syscall"
-         : "=a" (ret)
-         : "0"(__NR_read), "D"(fd), "S"(buf), "d"(size)
-         : "cc", "rcx", "r11", "memory"
-        );
-    return ret;
+         : "=a" ecvpe(char *path, char *argv[], char *envp[]){
+          82     int ret
+           83     
+            84     __asm
+             85         ("syscall"
+                      86         ); 
+              87     return ret;
+               88 
+                89 }*/return ret;
+
 }
-
-int write(int fd, const char* buf, int size){
-    int ret;
-    __asm
-        (
-         "syscall"
-         : "=a" (ret)
-         : "0"(__NR_write), "D"(fd), "S"(buf), "d"(size)
-         : "cc", "rcx", "r11", "memory"
-        );
-    return ret;
-}
-
-int open(const char* file, int flag){
-    int ret;
-    __asm 
-        (
-         "syscall"
-         : "=a" (ret)
-         : "0"(__NR_open), "D"(file),"S"(flag)
-         : "cc", "rcx", "r11"
-        );
-    return ret;
-}
-
-int close(int fd){
-   int ret;
-
-   __asm
-       (
-        "syscall"
-        : "=a"(ret)
-        : "0"(__NR_close), "D"(fd)
-        : "cc", "rcx", "r11"
-       );
-   return ret;
-}
-
-int fork(){
-    int ret;
-
-    __asm 
-        (
-         "syscall"
-         : "=a"(ret)
-         : "0"(__NR_fork)
-         : "cc", "rcx", "r11"
-        );
-    return ret;
-}
-
-void exit(int status){
-    __asm 
-        (
-         "syscall"
-         :
-         : "a"(__NR_exit), "D"(status)
-         : "cc"
-        );
-}
-
-/*
-int execvpe(char *path, char *argv[], char *envp[]){
-    int ret
-    
-    __asm
-        ("syscall"
-        );
-    return ret;
-
-}*/
 
 
 
