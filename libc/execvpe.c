ea39af4423ea182d103b7077aa6410f88bac306b
Leena Shekhar <leena.mesra@gmail.com>
2017-09-06 12:55:41 -0400

    execvpe.c

--- libc/execvpe.c
+++ libc/execvpe.c
@@ -3,27 +3,34 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
-
-// will use it to resolve the file
+// default path when PATH is NULL
 #define DEFAULT_PATH "/bin:/usr/bin"
-extern char **environ;
-
 
 int execve(const char *filename, char *const argv[], char *const envp[]);
 
-// implement execvpe to resole path first
 
-char* concat(const char *s1, const char *s2)
+// TODO remove this when strcat added to string.c
+char *my_strcat(char *dest, const char *src)
 {
-    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
-    //in real code you would check for errors in malloc here
-    strcpy(result, s1);
-    strcat(result, s2);
+    // allocate enough memory to dest else error
+    char *result = dest;
+
+    while (*dest)
+        dest++;
+    while (*dest++ = *src++)
+        ;
     return result;
 }
 
+char* concat(const char *s1, const char *s2)
+{
+    // length +1 for the NULL-terminator
+    char *result = malloc(strlen(s1)+strlen(s2)+1);
+    strcpy(result, s1);
+    my_strcat(result, s2);
+    return result;
+}
 
 int execvpe(const char *file, char *const argv[], char *const envp[]){
 
@@ -32,42 +39,49 @@ int execvpe(const char *file, char *const argv[], char *const envp[]){
         return -1;
     }
 
-    // don't search for the binary
+    int slash = 0;
+
+    // don't search for the binary; if fail set the slash flag
     if (strchr(file, '/') != NULL){
         int fd = open(file, 0);
-        if(fd < 0)
-            return -1;
-        execve(file, argv, envp);
+        if(fd > 0)
+            execve(file, argv, envp);
+        else
+            // set the flag and sarch in the PATH
+           slash = 1;
     }
 
-    // TODO replace getenv with custom setenv
+    // TODO replace getenv with custom setenv written in setenv.c
     char *path = getenv ("PATH");
     // if path is NULL use a default value
     if (!path)
         path=DEFAULT_PATH;
 
-    // filename and NULL
-    size_t file_len = strlen(file) + 1;
-    size_t path_len = strlen (path) + 1;
-
-    // max string size
-    char pbuffer[path_len + file_len + 1];
+    char pbuffer[strlen(path) + strlen(file) + 1];
     strcpy(pbuffer, path);
 
     // get each path and search for the binary
     char* token = strtok(pbuffer, ":");
     char delim = '/';
-
-    while (token) {
+    while (token){
         token = strtok(NULL, ":");
         // concat token and the actual path
-        char *citoken= concat(token, &delim);
-        char *ctoken = concat(citoken, file);
+        char *ctoken;
+        char *citoken = NULL;
+        if(slash != 1) {
+            citoken = concat(token, &delim);
+            ctoken = concat(citoken, file);
+        }
+        else
+            ctoken = concat(token, file);
+
         int fd = open(ctoken, 0);
         if (fd > 0) {
-        // pbuffer points to the binary
-            execve(ctoken, argv, envp);
             //printf("executing:%s\n", ctoken);
+            execve(ctoken, argv, envp);
+            if(citoken != NULL)
+                free(citoken);
+            free(ctoken);
             break;
         }
         else {
@@ -75,7 +89,7 @@ int execvpe(const char *file, char *const argv[], char *const envp[]){
             continue;
         }
     }
-
+    free(pbuffer);
 }
 
 void main(){
@@ -88,13 +102,11 @@ void main(){
         token = strtok(NULL, ":");
     }
 
+    // /bin/ls and /ls and ls work
     char *args[2];
-
-    // /bin/ls and ls work
     args[0] = "ls";
     args[1] = NULL;
 
     execvpe(args[0], args, NULL);
-
 };
 
