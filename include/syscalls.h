5655ff608df31586e1a9db4b9f14621c4df99dce
Leena Shekhar <leena.mesra@gmail.com>
2017-09-07 04:43:52 -0400

    I guess these two lines were missing in the header file

--- include/syscalls.h
+++ include/syscalls.h
@@ -1,3 +1,8 @@
+//got error so adding these lines
+#ifndef _SYSCALLS_H
+#define _SYSCALLS_H
+
+
 #include <sys/defs.h>
 #include <fcntl.h>
 
@@ -6,3 +11,5 @@ void* brk(void* addr);
 int wait4(pid_t upid, int* stat_addr, int options);
 int dup(int fd);
 int getdents(int fd, struct linux_dirent* dirent, int count);
+
+#endif
