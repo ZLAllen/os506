618a6446cef44d925f95cebefba2380ed0066554
zlallen <zhaoqi_li@yahoo.com>
2017-09-13 12:45:12 -0400

    add timer ticking routine, need to add timer setup

--- include/sys/ktime.h
+++ include/sys/ktime.h
@@ -1,6 +1,7 @@
 #ifndef _KTIME_H
 #define _KTIME_H
 
+#include <sys/defs.h>
 
 void addTick();
 
--- sys/ktime.c
+++ sys/ktime.c
@@ -1,3 +1,35 @@
 #include <sys/ktime.h>
 
+int ticks;
+int s_cnt; // every 5 seconds, we wait for 19 ticks
+uint64_t second;
+uint64_t day;
+
+void addTick(){
+    if(s_cnt < 5){
+        if(ticks < 18)
+            ++ticks;
+        else{
+            ++second;
+            ticks = 0;
+            ++s_cnt;
+        }
+    }else{
+        if(ticks < 19)
+            ++ticks;
+        else{
+            ++second;
+            ticks = 0;
+            s_cnt = 0;
+        }
+    }
+
+    if(second >= 86400){
+        day++;
+        second = 0;
+    }
+}
+
+
+
 
