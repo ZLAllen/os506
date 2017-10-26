6b41bff52a8edf3129d5ff327b5099679b5e64e1
zlallen <zhaoqi_li@yahoo.com>
2017-10-25 20:04:01 -0400

    filling the physical descriptor

--- include/sys/pmap.h
+++ include/sys/pmap.h
@@ -1,6 +1,7 @@
 #ifndef _PMAP_H_
 #define _PMAP_H_
 
+#include <sys/defs.h>
 
 struct smap_t {
     uint64_t base, length;
@@ -8,8 +9,11 @@ struct smap_t {
 }__attribute__((packed)) *smap;
 
 
+#define PGSIZE 0x1000
 
-pmap_init(uint32_t *modulep, void *physbase, void *physfree);
+#define ALIGN_UP(x) ((x & ~(PGSIZE-1)) + PGSIZE)
+
+void pmap_init(uint32_t *modulep, void *physbase, void *physfree);
 
 
 #endif
--- sys/pmap.c
+++ sys/pmap.c
@@ -1,20 +1,52 @@
-#include <pmap.h>
+#include <sys/pmap.h>
+#include <sys/kprintf.h>
 
 
-pmap_init(uint32_t *modulep, void *physbase, void *physfree)
+struct freelist_entry{
+    uint64_t base;
+    uint64_t next;
+};
+
+struct freelist_entry* freelist_head;
+
+void pmap_init(uint32_t *modulep, void *physbase, void *physfree)
 {
     // push all available smap_t pointers to an array, send the array to pmap init function
-
+/*
     uint64_t smap_arr[8];
-    char i = 0;
+    int num = 0, i;
     while(modulep[0] != 0x9001) modulep += modulep[1]+2;
 
-    for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) {
+    for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) 
+    {
     
-        if (smap->type == 1  && smap->length != 0) {
+        if (smap->type == 1  && smap->length != 0) 
+        {
             kprintf("Available Physical Memory [%p-%p]\n", smap->base, smap->base + smap->length);
-            smap_arr[i] = (uint64_t)smap;
+            if(i < 8)
+            {
+                smap_arr[num] = (uint64_t)smap;
+                num++;
+            }
+            
         }
     }
 
+    // skip the first 640KB
+    for(i = 1; i < num; ++i) 
+    {
+        smap = (struct smap_t*)smap_arr[i];
+        //extract mem
+        if(smap->length > PGSIZE) 
+        {
+            uint64_t start = smap->base;
+            uint64_t end = smap->base + smap->length;
+        }
+        
+    }
+
+
+*/
+
 }
+
