9598040b82116968f764b20e85a5d39a78a89a9c
Brian Yang <bryang@cs.stonybrook.edu>
2017-11-19 05:15:52 -0500

    Kernel context switch with 4 threads and local var test

--- sys/switch.c
+++ sys/switch.c
@@ -1,16 +1,19 @@
 #include <sys/switch.h>
 #include <sys/schedule.h>
 
-task_struct *task1, *task2;
+task_struct *task1, *task2, *task3, *task4;
 
 void thread1()
 {
-	kprintf("Thread 1\n");
+    int x = 100;
+	kprintf("Thread 1. Variable is %d.\n", x);
     run_next_task();
-    kprintf("Back in thread 1\n");
+    x++;
+    kprintf("Back in thread 1. Variable is %d.\n", x);
     run_next_task();
-
-    kprintf("Back again in thread 1\n");
+    x++;
+    kprintf("Back again in thread 1. Variable is %d.\n", x);
+    run_next_task();
     while(1) {}
 
    // set_tss_rsp((void*)(ALIGN_UP(task2->rsp) - 16));
@@ -19,13 +22,45 @@ void thread1()
 
 void thread2()
 {
-	kprintf("Thread 2 \n");
+    int x = 0;
+    x++;
+	kprintf("Thread 2. Variable is %d. \n", x);
     run_next_task();
-    kprintf("Back in thread 2\n");
+    x++;
+    kprintf("Back in thread 2. Variable is %d. \n", x);
     run_next_task();
-    kprintf("Back again in thread 2\n");
-    //switch_to(task2, task1);
-    //kprintf("Back again in thread 2\n");
+    x++;
+    kprintf("Back again in thread 2. Variable is %d.\n", x);
+    kprintf("Thread 2 will now run a while(1) and not call run_next_task().");
+
+    while(1) {}
+
+   // set_tss_rsp((void*)(ALIGN_UP(task1->rsp) - 16));
+    __asm__ volatile("retq");
+
+}
+
+void thread3()
+{
+	kprintf("Thread 3 \n");
+    run_next_task();
+    kprintf("Back in thread 3\n");
+    run_next_task();
+    kprintf("Back again in thread 3\n");
+
+    while(1) {}
+
+   // set_tss_rsp((void*)(ALIGN_UP(task1->rsp) - 16));
+    __asm__ volatile("retq");
+
+}
+void thread4()
+{
+	kprintf("Thread 4 \n");
+    run_next_task();
+    kprintf("Back in thread 4\n");
+    run_next_task();
+    kprintf("Back again in thread 4\n");
 
     while(1) {}
 
@@ -40,6 +75,8 @@ void init_thread()
 {
     task1 = create_new_task(&thread1);
     task2 = create_new_task(&thread2);
+    task3 = create_new_task(&thread3);
+    task4 = create_new_task(&thread4);
 
     /*
     task1 = get_task_struct();
@@ -59,6 +96,8 @@ void init_thread()
     task2->pid = 1;*/
 	schedule(task1);
     schedule(task2);
+    schedule(task3);
+    schedule(task4);
 
     run_next_task();
 
