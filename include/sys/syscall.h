365f0b854eb406db605af394b5c147ed7a1e6064
Brian Yang <bryang@cs.stonybrook.edu>
2017-12-07 22:48:19 -0800

    Add syscall function calls

---
+++ include/sys/syscall.h
@@ -0,0 +1,19 @@
+#include <sys/defs.h>
+#include <sys/syscalls.h>
+
+/**
+ * Kernel syscall utility functions
+ */
+
+uint64_t test(uint64_t arg);
+uint64_t fork();
+void exit();
+
+// utility functions to set arguments for syscall
+void syscallArg0(uint64_t num);
+void syscallArg1(uint64_t num, uint64_t arg0);
+void syscallArg2(uint64_t num, uint64_t arg0, uint64_t arg1);
+void syscallArg3(uint64_t num, uint64_t arg0, uint64_t arg1, uint64_t arg2);
+void syscallArg4(uint64_t num, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3);
+void syscallArg5(uint64_t num, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4);
+
---
+++ sys/syscall.c
@@ -0,0 +1,81 @@
+#include <sys/syscall.h>
+
+/**
+ * Kernel syscall utility functions
+ * so making syscalls aren't so tedious
+ */
+
+uint64_t test(uint64_t arg) {
+
+    uint64_t num = SYS_test;
+    uint64_t ret;
+    syscallArg1(num, arg);
+
+    __asm__ volatile ("int $0x80"
+        :"=r" (ret)
+        :: "%rbx", "%rcx", "%rdx", "%rsi", "%rdi"
+    ); 
+
+    return ret;
+}
+
+uint64_t fork() {
+    uint64_t num = SYS_fork;
+    uint64_t ret;
+
+    syscallArg0(num);
+
+    __asm__ volatile ("int $0x80"
+        :"=r" (ret)
+        :: "%rbx", "%rcx", "%rdx", "%rsi", "%rdi"
+    ); 
+    return ret;
+}
+
+void exit() {
+    uint64_t num = SYS_exit;
+
+    syscallArg0(num);
+
+    __asm__ volatile ("int $0x80"
+        ::: "%rbx", "%rcx", "%rdx", "%rsi", "%rdi"
+    ); 
+}
+
+void syscallArg0(uint64_t num) {
+    __asm__ __volatile__
+        ("movq %0, %%rax" :: "r" (num));
+}
+
+void syscallArg1(uint64_t num, uint64_t arg0) {
+    __asm__ __volatile__
+        ("movq %0, %%rax" :: "r" (num));
+    __asm__ __volatile__
+        ("movq %0, %%rbx" ::"r" (arg0));
+}
+
+void syscallArg2(uint64_t num, uint64_t arg0, uint64_t arg1) {
+    __asm__ __volatile__
+        ("movq %0, %%rax" :: "r" (num));
+    __asm__ __volatile__
+        ("movq %0, %%rbx;" 
+         "movq %1, %%rcx;"
+         ::"r" (arg0), "r" (arg1)
+        );
+}
+
+void syscallArg3(uint64_t num, uint64_t arg0, uint64_t arg1, uint64_t arg2) {
+    __asm__ __volatile__
+        ("movq %0, %%rax" :: "r" (num));
+    __asm__ __volatile__
+        ("movq %0, %%rbx;" 
+         "movq %1, %%rcx;"
+         "movq %2, %%rdx;"
+         ::"r" (arg0), "r" (arg1), "r" (arg2)
+        );
+}
+
+void syscallArg4(uint64_t num, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3) {
+}
+void syscallArg5(uint64_t num, uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4) {
+}
