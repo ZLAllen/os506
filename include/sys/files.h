3487ef504bbe2918a95a8d4ce5e68b2c3ca46087
Leena Shekhar <leena.mesra@gmail.com>
2017-12-12 22:59:01 -0500

    forogt to commit this

---
+++ include/sys/files.h
@@ -0,0 +1,44 @@
+#ifndef FS_H
+#define FS_H
+
+#include <sys/defs.h>
+
+enum ftype {DIRECTORY, FILE};
+
+struct file 
+{
+    struct file_ops *fop; 
+    unsigned long count;      
+    off_t offset;           
+    void *data;   
+    unsigned long size;
+	struct file* free;
+	int flags; //this is needed to set w/r perm
+};
+
+
+struct file_ops 
+{ 
+    struct file* (*open) (const char *, int pflags);
+    ssize_t (*read) (struct file *, char *, size_t, off_t *);
+    int (*close) (struct file *);
+	int (*readdir) (struct file *, void *, unsigned int);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
