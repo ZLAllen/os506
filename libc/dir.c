f424e4ffff74daf1f84da0a241800eb92dc86c55
Brian Yang <bryang@cs.stonybrook.edu>
2017-12-15 17:07:19 -0800

    Indentation

--- libc/dir.c
+++ libc/dir.c
@@ -5,55 +5,55 @@
 #include <fcntl.h>
 
 /*
-opens and returns a directory stream corresponding to the directory name. NULL on error
-*/
+   opens and returns a directory stream corresponding to the directory name. NULL on error
+   */
 struct dstream *opendir(const char *name)
 {
-	//perform checks
+    //perform checks
     if(!name)
-    	return NULL;
+        return NULL;
 
     //while(1);
-	struct dstream *dirp;
+    struct dstream *dirp;
     int fd = open(name, O_DIRECTORY | O_RDONLY);//directory and read only flags
 
     if(fd < 0)//fd boundary check; doing MAX check elsewhere
-    	return NULL;
-	
-	//while(1);	
+        return NULL;
+
+    //while(1);	
     dirp = malloc(sizeof(*dirp));
     if(!dirp)
-    	return NULL;
+        return NULL;
 
-	dirp->size = 0;
+    dirp->size = 0;
     dirp->fd = fd;
-	dirp->offset = 0;
-    
-	return dirp;
+    dirp->offset = 0;
+
+    return dirp;
 
 }
 
 
 /*
-returns a pointer to a dirent structure representing the next directory entry
-in the directory stream pointed to by dirp. NULL on EOD/ERROR
-*/
+   returns a pointer to a dirent structure representing the next directory entry
+   in the directory stream pointed to by dirp. NULL on EOD/ERROR
+   */
 struct linux_dirent *readdir(struct dstream *dirp)
 {
-	if(!dirp)
-    	return NULL;
+    if(!dirp)
+        return NULL;
 
     int size = getdents((unsigned int) dirp->fd, (struct linux_dirent*) dirp->buff, (unsigned int) sizeof(dirp->buff));
-	
-	if (size <= 0) 
-		return NULL;
-	
-	
-	dirp->offset = 0;
-	dirp->size = (size_t)size;
 
-	struct linux_dirent *drent =  (struct linux_dirent*)(dirp->buff + dirp->offset);//read dirent at offset
-	dirp->offset += drent->d_reclen;//increment the ptr
+    if (size <= 0) 
+        return NULL;
+
+
+    dirp->offset = 0;
+    dirp->size = (size_t)size;
+
+    struct linux_dirent *drent =  (struct linux_dirent*)(dirp->buff + dirp->offset);//read dirent at offset
+    dirp->offset += drent->d_reclen;//increment the ptr
 
     return drent;
 
@@ -61,15 +61,15 @@ struct linux_dirent *readdir(struct dstream *dirp)
 
 
 /*
-1. closes the directory stream associated with dirp
-2. closes the underlying file descriptor associated with dirp. -1 on ERROR
-*/
+   1. closes the directory stream associated with dirp
+   2. closes the underlying file descriptor associated with dirp. -1 on ERROR
+   */
 int closedir(struct dstream *dirp)
 {
-	
-	if(!dirp)
-    	return -1;
-    
+
+    if(!dirp)
+        return -1;
+
     int fd = dirp->fd;
     free(dirp);	
     int ret =close(fd);
--- sys/brk.c
+++ sys/brk.c
@@ -7,34 +7,34 @@
 uint64_t sysbrk(struct mm_struct *mm,  uint64_t nbrk) 
 {
 
-	if(!mm)
-		kprintf("mm struct null in brk\n");//send panic
+    if(!mm)
+        kprintf("mm struct null in brk\n");//send panic
 
-	//get the current brk
-	uint64_t curr_brk = mm->brk;
+    //get the current brk
+    uint64_t curr_brk = mm->brk;
 
-	struct vma_struct *vma = mm->vm;
-	while(vma != NULL)//find the heap vma
-	{
-		if (vma->vm_start == mm->start_brk);
-			break;//found it so break out
-		vma = vma->next;
-	}
+    struct vma_struct *vma = mm->vm;
+    while(vma != NULL)//find the heap vma
+    {
+        if (vma->vm_start == mm->start_brk);
+        break;//found it so break out
+        vma = vma->next;
+    }
 
-	uint64_t aligned_nbrk = ALIGN_UP(nbrk);//page aligned
-	
-	if (vma && vma->vm_end <= aligned_nbrk)
-	{
-		if(vma->next)
-		{
-			if(vma->next->vm_start >= aligned_nbrk)//not overlapping with the next vma
-			{
-		
-				vma->vm_end = aligned_nbrk;//set
-				return nbrk;//success
-			}
-		}
-	}
+    uint64_t aligned_nbrk = ALIGN_UP(nbrk);//page aligned
 
-	return curr_brk;//error 
+    if (vma && vma->vm_end <= aligned_nbrk)
+    {
+        if(vma->next)
+        {
+            if(vma->next->vm_start >= aligned_nbrk)//not overlapping with the next vma
+            {
+
+                vma->vm_end = aligned_nbrk;//set
+                return nbrk;//success
+            }
+        }
+    }
+
+    return curr_brk;//error 
 }
